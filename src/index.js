49f50d2e90a53f9154c824eb9e21499e416774f9
Fadhil <fadhildarma13@gmail.com>
2022-06-15 21:08:41 +0700

    change

--- src/index.js
+++ src/index.js
@@ -7,13 +7,6 @@ import store from "./lib/store";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-import { Provider } from "react-redux";
-import { store } from "./store";
-
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
